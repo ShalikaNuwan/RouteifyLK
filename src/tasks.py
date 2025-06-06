03467faac2696bc29ed5325f722ce1bd8e698ec3
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-06-06 12:10:01 +0530

    updated the task to extract information from the user

--- src/tasks.py
+++ src/tasks.py
@@ -1,3 +1,17 @@
 from crewai import Task
-from llm import llm
-from agents import agents
+from agents import trip_details_agent
+
+trip_data_extractor = Task(
+    description= "extract the trip details from the input prompt {input} given by the user.",
+    expected_output= (
+        "detailed output of the time period in sri lanka,"
+        "the locations where the user wants to go,"
+        "prefferes hotels / lodges, meal preferences,"
+        "Total budget,"
+    ),
+    agent=trip_details_agent
+)
+
+tasks: list[Task] = [trip_data_extractor]
+
+__all__ = ["tasks"]
