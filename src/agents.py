420171d45e78b33ce0478d03378913f9aca5688b
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-06-07 12:16:00 +0530

    removed the agents and tasks code since yaml file is used

--- src/agents.py
+++ src/agents.py
@@ -2,25 +2,16 @@ from crewai import Agent
 from llm import llm
 from crewai.tools import tool
 
-@tool('getUserInput')
-def getInputFromTheUser() -> str:
-    "User will give the input for the trip details"
-    user_input = "I want to go to the kandy, Colombo, badulla, Ella. I will stay in sri lanka from 15th of june to 23th of june. my total budhet will be 5000$. I Preferred to use a van for the transportation."
-    # user_input = input('Enter the trip details') 
-    return user_input
+# trip_details_agent = Agent(
+#     role="Understand the user preferences for the trip.",
+#     goal=(
+#         "Extract the trip details of {input_details} from the user, including the cities they want to visit, the duration of the trip, total buddget, Hotel type, meal preferences and any specific activities they want to do. "
+#     ),
+#     backstory="You are a smart assistant to understand the user preferences for the trip.",
+#     llm=llm,
+#     tools=[]
+# )
 
+# agents: list[Agent] = [trip_details_agent]
 
-#Agent to understand the user preferenes for the trip.
-trip_details_agent = Agent(
-    role="Understand the user preferences for the trip.",
-    goal=(
-        "Extract the trip details of {input_details} from the user, including the cities they want to visit, the duration of the trip, total buddget, Hotel type, meal preferences and any specific activities they want to do. "
-    ),
-    backstory="You are a smart assistant to understand the user preferences for the trip.",
-    llm=llm,
-    tools=[getInputFromTheUser]
-)
-
-agents: list[Agent] = [trip_details_agent]
-
-__all__ = ["agents", "trip_details_agent"]
+# __all__ = ["agents", "trip_details_agent"]
--- src/tasks.py
+++ src/tasks.py
@@ -1,17 +1,17 @@
 from crewai import Task
 from agents import trip_details_agent
 
-trip_data_extractor = Task(
-    description= "extract the trip details from the input prompt {input} given by the user.",
-    expected_output= (
-        "detailed output of the time period in sri lanka,"
-        "the locations where the user wants to go,"
-        "prefferes hotels / lodges, meal preferences,"
-        "Total budget,"
-    ),
-    agent=trip_details_agent
-)
+# trip_data_extractor = Task(
+#     description= "extract the trip details from the input prompt {input_details} given by the user.",
+#     expected_output= (
+#         "detailed output of the time period in sri lanka,"
+#         "the locations where the user wants to go,"
+#         "prefferes hotels / lodges, meal preferences,"
+#         "Total budget,"
+#     ),
+#     agent=trip_details_agent
+# )
 
-tasks: list[Task] = [trip_data_extractor]
+# tasks: list[Task] = [trip_data_extractor]
 
-__all__ = ["tasks"]
+# __all__ = ["tasks"]
