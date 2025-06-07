648d938743602e6c0ab4add9a96d5e53c10b08ce
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-06-07 12:14:53 +0530

    changed the crew.py using the yaml files

--- src/crew.py
+++ src/crew.py
@@ -1,11 +1,44 @@
-from crewai import Crew
-from agents import agents
-from tasks import tasks
+from crewai import Crew,Agent,Task,Process
+from crewai.project import CrewBase,agent,task,crew
+from llm import llm
+import yaml
 
-crew = Crew(
-    agents=agents,
-    tasks=tasks,
-    verbose=True
-)
+def load_yaml(path):
+    with open(path, 'r') as file:
+        return yaml.safe_load(file)
 
-crew.kickoff()
+@CrewBase
+class TripPlannerCrew:
+    "The whole crew planing the trip"
+    agent_config = load_yaml('src/config/agents.yaml')
+    task_config = load_yaml('src/config/tasks.yaml')
+    
+    @agent
+    def trip_details_agent(self) -> Agent:
+        return Agent(
+            config= self.agent_config['trip_details_agent'],
+            tools=[],
+            llm=llm
+        )
+        
+    @task
+    def trip_data_extractor(self) -> Task:
+        return Task(
+            config=self.task_config['trip_data_extractor'],
+            agent=self.trip_details_agent()
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        "Create the Trip planner Crew"
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True
+        )
+
+    
+input = "I want to go to the kandy, Colombo, badulla, Ella. I will stay in sri lanka from 15th of june to 23th of june. my total budhet will be 5000$. I Preferred to use a van for the transportation."
+
+TripPlannerCrew().crew().kickoff(inputs={"input_details": input})
