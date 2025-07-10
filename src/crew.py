81af792f2548a6498242aff0d998b3d89c4c81ba
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-07-10 14:05:18 +0530

    added the data structure for the locations

--- src/crew.py
+++ src/crew.py
@@ -1,8 +1,11 @@
 from crewai import Crew,Agent,Task,Process
 from crewai.project import CrewBase,agent,task,crew
-from tools import DirectionMapTool
-from llm import llm
+from crewai_tools import SerperDevTool
+from .tools import DirectionMapTool
+from .llm import llm
 import yaml
+from pydantic import BaseModel
+from typing import List
 
 def load_yaml(path):
     try:  
@@ -14,6 +17,26 @@ def load_yaml(path):
     except Exception as e:  
         print(f"Error loading YAML: {e}")  
         return {} 
+
+class Attraction(BaseModel):
+    name: str
+    distance: str
+    time_to_visit: str
+    cost: str
+
+class Restaurant(BaseModel):
+    name: str
+    meal_cost: str
+    specialty: str
+
+class Location(BaseModel):
+    city: str
+    attractions: List[Attraction]
+    restaurants: List[Restaurant]
+
+class OutputModel(BaseModel):
+    locations: List[Location]
+
 
 @CrewBase
 class TripPlannerCrew:
@@ -37,7 +60,15 @@ class TripPlannerCrew:
             tools=[direction_tool],
             llm=llm
         )
-        
+    
+    @agent
+    def location_details_agent(self) -> Agent:
+        return Agent(
+            config=self.agent_config['location_details_aegnt'],
+            tools=[SerperDevTool()],
+            llm=llm
+        )
+    
     @task
     def trip_data_extractor(self) -> Task:
         return Task(
@@ -53,6 +84,17 @@ class TripPlannerCrew:
             context=[self.trip_data_extractor()]
         )
     
+    @task
+    def find_locations_resturant(self) -> Task:
+        return Task(
+            config=self.task_config['find_locations_resturants'],
+            agent=self.location_details_agent(),
+            context=[self.find_best_path(),self.trip_data_extractor()],
+            output_json=OutputModel,
+            output_file='locations.json'
+            
+        )
+    
     @crew
     def crew(self) -> Crew:
         "Create the Trip planner Crew"
@@ -63,7 +105,5 @@ class TripPlannerCrew:
             verbose=True
         )
 
-    
-input = "I want to go to the kandy, Colombo, badulla, Ella. I will stay in sri lanka from 15th of june to 23th of june. my total budhet will be 5000$. I Preferred to use a van for the transportation."
-
-TripPlannerCrew().crew().kickoff(inputs={"input_details": input})
+def runplanner(input):
+    TripPlannerCrew().crew().kickoff(inputs={"input_details": input})
